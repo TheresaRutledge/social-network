ea93661a1e6311cfc1265a61e7d37fbea44f332b
Theresa Rutledge <t.roserutledge@gmail.com>
2020-09-17 13:49:42 -0700

    user and thought models added

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.reduce((total, friend) => total + friend.replies.length + 1, 0);
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
+
